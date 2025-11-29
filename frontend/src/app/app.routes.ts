6f329c0ab8227a8d84633b5aff404939fd4195b6
syntax-weaver <starb3936@gmail.com>
2025-11-29 10:31:26 +0200

    navbar and footer and main-layout and auth-layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,34 @@ import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
 import { PatientProfile } from './patient-profile/patient-profile';
 import { PaymentFormComponent } from './payments/payment-form/payment-form';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
+
+// export const routes: Routes = [
+//     {path: '', redirectTo: 'login', pathMatch: 'full' },
+//     {path: 'login', component: Login },
+//     {path: 'register', component: Register },
+//     {path: 'patient-profile', component: PatientProfile },
+//     {path: 'payment-form', component:PaymentFormComponent}
+// ];
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full' },
-    {path: 'login', component: Login },
-    {path: 'register', component: Register },
-    {path: 'patient-profile', component: PatientProfile },
-    {path: 'payment-form', component:PaymentFormComponent}
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: 'patient-profile', component: PatientProfile },
+      { path: 'payment-form', component: PaymentFormComponent },
+      // add other "app" pages here
+    ]
+  }
 ];
