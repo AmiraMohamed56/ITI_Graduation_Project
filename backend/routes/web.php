252867dbb5ac0c8f74ae8130aa5a6a9818a06670
AmiraMohamed56 <awadet3469@gmail.com>
2025-11-29 06:25:35 +0200

    restore deleted routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\AdminSettingsController;
+
 Route::get('/', function () {
     return view('Doctors_Dashboard.schedule.show');
 });
@@ -16,5 +18,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+// =========================================== admin start =====================================================
+// Route::get('/test-flash', function () {
+//     return redirect('/admin/settings')->with('success', 'It works!');
+// });
+Route::get('admin/settings', [AdminSettingsController::class, 'edit'])->name('admin.settings.edit');
+Route::patch('admin/settings', [AdminSettingsController::class, 'update'])->name('admin.settings.update');
 
+// ===========================================  admin end  =====================================================
 require __DIR__.'/auth.php';
