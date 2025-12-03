c2eea50d5cce9b014fc6e9a5519c6d5f074faad5
AmiraMohamed56 <awadet3469@gmail.com>
2025-12-03 15:09:13 +0200

    add Reviews Apis

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Patient\PatientApiController;
 use App\Http\Controllers\Api\Doctor\DoctorController;
+use App\Http\Controllers\Api\Reviews\ReviewController;
 
 // API Routes for Patient profile management
 Route::get('patient', [PatientApiController::class, 'index']);
@@ -13,3 +14,4 @@ Route::delete('patient/{id}/profile-pic', [PatientApiController::class, 'removeP
 
 //Api Routes for Doctor management
 Route::apiResource('doctors', DoctorController::class);
+Route::apiResource('reviews', ReviewController::class);
