2dd4f3dd4ee0444bf811605be63da4d81b73bdef
AmiraMohamed56 <awadet3469@gmail.com>
2025-12-04 11:25:54 +0200

    Fix case issue for UpdatePatientRequest

--- backend/app/Http/Requests/Patients/UpdatePatientRequest.php
+++ backend/app/Http/Requests/Patients/UpdatePatientRequest.php
@@ -1,21 +1,28 @@
 <?php
 
-namespace App\Http\Requests\Patients;
+namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdatePatientRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
     public function rules(): array
     {
         return [
-            'blood_type' => 'sometimes|nullable|string|max:5',
-            'chronic_diseases' => 'sometimes|nullable|string',
+            //
         ];
     }
 }
